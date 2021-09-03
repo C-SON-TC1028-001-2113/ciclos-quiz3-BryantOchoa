6885b7d893cea8c95839ffdeed456b5910edcd11
BryantOchoa <A01252419@itesm.mx>
2021-09-03 18:13:31 +0000

    quiz 3 bryant

--- assignments/05PromedioSencillo/src/exercise.py
+++ assignments/05PromedioSencillo/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-
+     x = int(input())
+     suma = 0 
+     for i in range(x):
+        y = float(input())
+        suma = suma + y
+        promedio = suma / x
+     print(promedio)
 if __name__=='__main__':
     main()
